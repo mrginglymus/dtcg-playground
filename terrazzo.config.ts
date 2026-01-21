3049fe5f6638e6be14ddbee0ef19420ed018eccf
Bill Collins <bill@bill.works>
2026-01-21 10:46:28 +0000

    updates

--- terrazzo.config.ts
+++ terrazzo.config.ts
@@ -1,24 +1,9 @@
 import { defineConfig } from "@terrazzo/cli";
 import css from "@terrazzo/plugin-css";
 import sass from "@terrazzo/plugin-sass";
-import {
-  BuildHookOptions,
-  Plugin,
-  TransformHookOptions,
-} from "@terrazzo/parser";
-
-const MyPlugin: Plugin = {
-  name: "My Plugin",
-  async transform(options: TransformHookOptions): Promise<void> {
-    console.log(options);
-  },
-  async build(options: BuildHookOptions): Promise<void> {
-    console.log(options);
-  },
-};
 
 export default defineConfig({
   tokens: ["./resolver.json"],
-  plugins: [css(), sass(), MyPlugin],
+  plugins: [css(), sass()],
   outDir: "./dist/",
 });
