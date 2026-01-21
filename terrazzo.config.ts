6b27b4eb9ab7ccd2d24e00ce860e24c5384dccbd
Bill Collins <bill@bill.works>
2026-01-21 14:05:29 +0000

    allperms

--- terrazzo.config.ts
+++ terrazzo.config.ts
@@ -7,54 +7,35 @@ import { makeCSSVar } from "@terrazzo/token-tools/css";
 export default defineConfig({
   tokens: ["./resolver.json"],
   plugins: [
-    css(),
+    css({
+      permutations: [
+        {
+          input: { theme: "orange", mode: "light" },
+          prepare(css: string): string {
+            return `.orange.light {${css}}`;
+          },
+        },
+        {
+          input: { theme: "orange", mode: "dark" },
+          prepare(css: string): string {
+            return `.orange.dark {${css}}`;
+          },
+        },
+        {
+          input: { theme: "blue", mode: "light" },
+          prepare(css: string): string {
+            return `.blue.light {${css}}`;
+          },
+        },
+        {
+          input: { theme: "blue", mode: "dark" },
+          prepare(css: string): string {
+            return `.blue.dark {${css}}`;
+          },
+        },
+      ],
+    }),
     sass(),
-    {
-      name: "my cool plugin",
-      build(options: BuildHookOptions): Promise<void> {
-        console.log(options);
-        const rawTokens = options.getTransforms({ format: "css" });
-        const rootDecls = [];
-        for (const [setName, set] of Object.entries(
-          options.resolver.source.sets,
-        )) {
-          for (const source of set.sources) {
-            for (const tokenName of Object.keys(source)) {
-              if (tokenName.startsWith("$")) {
-                continue;
-              }
-              const token = rawTokens.find((t) => t.id === tokenName)!;
-              rootDecls.push(`${makeCSSVar(token.localID)}: ${token.value};`);
-            }
-          }
-        }
-
-        for (const [modifierName, modifier] of Object.entries(
-          options.resolver.source.modifiers,
-        )) {
-          console.log(modifierName);
-          for (const [contextName, context] of Object.entries(
-            modifier.contexts,
-          )) {
-            const modifierTokens = options.getTransforms({
-              format: "css",
-              input: {
-                theme: "orange",
-                mode: "light",
-                [modifierName]: contextName,
-              },
-              id: Object.keys(context[0]),
-            });
-            console.log(contextName, Object.keys(context[0]));
-            console.log("xxx", modifierTokens);
-          }
-        }
-
-        console.log(rootDecls);
-        // console.log(rawTokens);
-        return;
-      },
-    },
   ],
   outDir: "./dist/",
 });
