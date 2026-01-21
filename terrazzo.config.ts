54bb383148cf488d5ad4dfd8cb6aea698de5eac5
Bill Collins <bill@bill.works>
2026-01-21 15:19:06 +0000

    yay

--- terrazzo.config.ts
+++ terrazzo.config.ts
@@ -1,39 +1,55 @@
 import { defineConfig } from "@terrazzo/cli";
-import css from "@terrazzo/plugin-css";
+import css, { Permutation } from "@terrazzo/plugin-css";
 import sass from "@terrazzo/plugin-sass";
-import { BuildHookOptions } from "@terrazzo/parser";
-import { makeCSSVar } from "@terrazzo/token-tools/css";
+import { parse, ResolverModifier } from "@terrazzo/parser";
+import fs from "node:fs/promises";
+
+const { resolver, tokens } = await parse({
+  filename: new URL("./resolver.json", import.meta.url),
+  src: await fs.readFile("./resolver.json", "utf-8"),
+});
+
+const permutations: Permutation[] = [];
+const allTokens = new Set(Object.keys(tokens));
+const permutationTokens = new Set<string>();
+
+for (const [modifier, { contexts }] of Object.entries<ResolverModifier>(
+  resolver.source.modifiers!,
+)) {
+  const modifierTokens = new Set(Object.keys(Object.values(contexts)[0][0]));
+  const modifierTokensExclude = allTokens.difference(modifierTokens);
+  modifierTokens.values().forEach((t) => permutationTokens.add(t));
+  for (const name of Object.keys(contexts)) {
+    permutations.push({
+      input: {
+        [modifier]: name,
+      },
+      prepare(css: string): string {
+        return `.${modifier}-${name} {
+          ${css}
+        }
+        `;
+      },
+      ignore: [...modifierTokensExclude],
+    });
+  }
+}
+
+permutations.unshift({
+  input: {},
+  ignore: [...permutationTokens],
+  prepare(css: string): string {
+    return `:root { 
+    ${css} 
+    }`;
+  },
+});
 
 export default defineConfig({
   tokens: ["./resolver.json"],
   plugins: [
     css({
-      permutations: [
-        {
-          input: { theme: "orange", mode: "light" },
-          prepare(css: string): string {
-            return `.orange.light {${css}}`;
-          },
-        },
-        {
-          input: { theme: "orange", mode: "dark" },
-          prepare(css: string): string {
-            return `.orange.dark {${css}}`;
-          },
-        },
-        {
-          input: { theme: "blue", mode: "light" },
-          prepare(css: string): string {
-            return `.blue.light {${css}}`;
-          },
-        },
-        {
-          input: { theme: "blue", mode: "dark" },
-          prepare(css: string): string {
-            return `.blue.dark {${css}}`;
-          },
-        },
-      ],
+      permutations,
     }),
     sass(),
   ],
