3ee7d6c6a17f781ea9cf939dc2280d0277028d0b
Bill Collins <bill@bill.works>
2026-05-05 15:37:02 +0100

    simple

--- terrazzo.config.ts
+++ terrazzo.config.ts
@@ -1,60 +1,22 @@
 import { defineConfig } from "@terrazzo/cli";
-import css, { Permutation } from "@terrazzo/plugin-css";
-import sass from "@terrazzo/plugin-sass";
-import { parse, ResolverModifier } from "@terrazzo/parser";
-import fs from "node:fs/promises";
-
-const { resolver, tokens } = await parse({
-  filename: new URL("./resolver.json", import.meta.url),
-  src: await fs.readFile("./resolver.json", "utf-8"),
-});
-
-const permutations: Permutation[] = [];
-const allTokens = new Set(Object.keys(tokens));
-const permutationTokens = new Set<string>();
-
-for (const [modifier, { contexts }] of Object.entries<ResolverModifier>(
-  resolver.source.modifiers!,
-)) {
-  const modifierTokens = new Set(Object.keys(Object.values(contexts)[0][0]));
-  const modifierTokensExclude = allTokens.difference(modifierTokens);
-  modifierTokens.values().forEach((t) => permutationTokens.add(t));
-  for (const name of Object.keys(contexts)) {
-    permutations.push({
-      input: {
-        [modifier]: name,
-      },
-      prepare(css: string): string {
-        return `
-        /* ignore: ${[...modifierTokensExclude].join(", ")} */
-        .${modifier}-${name} {
-          ${css}
-        }
-        `;
-      },
-      ignore: [...modifierTokensExclude],
-    });
-  }
-}
-
-permutations.unshift({
-  input: {},
-  ignore: [...permutationTokens],
-  prepare(css: string): string {
-    return `/* ignore: ${[...permutationTokens].join(", ")} */
-    :root { 
-    ${css} 
-    }`;
-  },
-});
+import css from "@terrazzo/plugin-css";
 
 export default defineConfig({
   tokens: ["./resolver.json"],
   plugins: [
     css({
-      permutations,
+      permutations: [
+        ...["light", "dark"].flatMap((mode) =>
+          ["orange", "blue"].map((theme) => ({
+            input: {
+              mode,
+              theme,
+            },
+            prepare: (css: string) => `.${theme}-${mode} {\n  ${css}\n}`,
+          })),
+        ),
+      ],
     }),
-    sass(),
   ],
   outDir: "./dist/",
 });
